9bd0581088b284028bced65c180a68545c7f96a7
unknownaloy <42836070+unknownaloy@users.noreply.github.com>
2022-08-31 09:23:23 +0100

    first commit

---
+++ kloud-stack-weather-react/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Cloud from './Cloud'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Cloud />
+    </div>
+  )
+}
+
+export default App
